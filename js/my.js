f0732833980634a841961ed74402ab3ce4db2db2
Dima Esipenko <mif83@mail.ru>
2017-03-24 16:57:28 +0200

    something add

--- js/my.js
+++ js/my.js
@@ -1,3 +1,27 @@
 /**
  * Created by dima on 24.03.2017.
  */
+/*// addEventListener, closure, setAttribute, removeAttribute, removeEventListener, querySelectorAll, querySelector
+
+$('.items').find('.items--subitems').on('click', function (event) {
+    console.log(event.target.textContent);
+}).each(function (elem, index) {
+    elem.textContent = index;
+}).attr('title', 'qwewqeqe');
+
+$('.delete-button').on('click', function () {
+    $('.items--subitems').off('click');
+});
+
+
+// find, on, off, each, attr, toggleClass, addClass, removeClass*/
+
+
+function $(str){
+    let arr = document.querySelectorAll(str);
+
+    if (arr.length > 1) return arr;
+    if (arr.length === 1) return arr[0];
+    if (arr.length === 0) return null;
+
+}
