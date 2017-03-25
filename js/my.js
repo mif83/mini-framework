125545ca490fe7bcbc011860381e50b877c21f71
Dima Esipenko <mif83@mail.ru>
2017-03-25 23:06:26 +0200

    add init, find

--- js/my.js
+++ js/my.js
@@ -16,11 +16,73 @@ $('.delete-button').on('click', function () {
 
 // find, on, off, each, attr, toggleClass, addClass, removeClass*/
 
-function $(str){
-    let arr = document.querySelectorAll(str);
 
-    if (arr.length > 1) return arr;
-    if (arr.length === 1) return arr[0];
-    if (arr.length === 0) return null;
+var $ = (function () {
+    this.eventMemorizer = {};
+    function myPlugin(params) {
+        if (!(this instanceof myPlugin)) {
+            return new myPlugin(params);
+        }
+        this.init.apply(this, arguments);
+    }
+    function flatten(arr, param){
+        console.log("flatten ",arr);
+        let result = [];
+        arr.forEach( item => {
+            if(Array.isArray(item)){
+                result = result.concat(item);
+            } else{
+                result.push(item);
+            }
+        });
+        if (!param){
+            result.forEach(item => {
+                if(Array.isArray(item)){
+                    result = flatten(result);
+                }
+            });
+        }
+        return result;
+    }
+    myPlugin.prototype = {
+        init: function (str) {
+            this.elems = Array.prototype.slice.call(document.querySelectorAll(str));
+        },
+        find : function (str) {
+          this.elems = flatten(this.elems
+                                .map( item => Array.prototype.slice.call( item.querySelectorAll(str) ) )
+                        );
 
-}
+            return this;
+        },
+        on : function (strEvent , callback){
+
+            this.elems.forEach(item => {
+                if(!item.memorize){
+                    item.memorize = {};
+                }
+                if (!item.memorize[strEvent]){
+                    item.memorize[strEvent] = [];
+                }
+                item.memorize[strEvent].push(callback);
+
+                item.addEventListener(strEvent, callback)
+            });
+            return this;
+        },
+        off : function (strEvent) {
+            this.elems.forEach(item => {
+                if (item.memorize[strEvent]){
+                    item.memorize[strEvent].forEach( arrFunc => {
+                        console.log(item);
+                        item.removeEventListener("strEvent", arrFunc);
+                    });
+                    item.memorize[strEvent].length = 0;
+                }
+            })
+        }
+    };
+    return myPlugin;
+}());
+
+
